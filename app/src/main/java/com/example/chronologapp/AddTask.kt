82d308602b3d3e2b92accaaa0bcd8403a823b2cc
JMDavids <104350944+JMDavids@users.noreply.github.com>
2024-04-30 13:53:13 +0200

    Renamed "add task" to "add timesheet"

--- app/src/main/java/com/example/chronologapp/AddTask.kt
+++ app/src/main/java/com/example/chronologapp/AddTask.kt
@@ -10,7 +10,7 @@ class AddTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_task)
+        setContentView(R.layout.activity_add_timesheet)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
