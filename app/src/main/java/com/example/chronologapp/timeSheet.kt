da4805199e7e3bdf99907cf2c65ad404e615e6a5
Ruben <rubengerber605@gmail.com>
2024-04-30 13:50:52 +0200

    added the timeSheet class (Object)

---
+++ app/src/main/java/com/example/chronologapp/timeSheet.kt
@@ -0,0 +1,8 @@
+package com.example.chronologapp
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+class timeSheet (val date:LocalDate,val startTime: LocalTime,val endTime: LocalTime,val description:String,val photoGraph:String?=null)
+
+
