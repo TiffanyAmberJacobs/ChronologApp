cf8adcda13ab45022079fff7e87ecd77d96240f0
JMDavids <104350944+JMDavids@users.noreply.github.com>
2024-05-01 13:52:53 +0200

    Added App Data

---
+++ app/src/main/java/com/example/chronologapp/AppData.kt
@@ -0,0 +1,8 @@
+package com.example.chronologapp
+
+class AppData {
+    companion object {
+        var categories: ArrayList<Category> = ArrayList()
+        var timesheets : ArrayList<timeSheet> = ArrayList()
+    }
+}
